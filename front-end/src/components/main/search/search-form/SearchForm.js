b0fb5ec3ad49531e6f15a7b1ad09da34324e0feb
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-03-08 17:47:34 -0300

    feat: add form to submit the move name

--- front-end/src/components/main/search/search-form/SearchForm.js
+++ front-end/src/components/main/search/search-form/SearchForm.js
@@ -1,13 +1,40 @@
 import { Button } from "@ui5/webcomponents-react";
 import { Input } from "@ui5/webcomponents-react";
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
 
 import "./search-form.scss";
 
+import React from "react";
+import axios from "axios";
+
 export default function SearchForm() {
+  const [moveName, setMoveName] = React.useState("");
+
+  function sendMoveName(e) {
+    e.preventDefault();
+    const url = "http://localhost:4000/";
+    const body = { moveName };
+    axios
+      .post(url, body)
+      .then(setMoveName(""))
+      .catch((err) => {
+        alert(err.response);
+        setMoveName("");
+      });
+  }
+
   return (
-    <form className="search-form">
-      <Input className="custom-input" />
-      <Button className="custom-button">Search</Button>
+    <form className="search-form" onSubmit={sendMoveName}>
+      <Input
+        className="custom-input"
+        type="text"
+        required
+        value={moveName}
+        onChange={(e) => setMoveName(e.target.value)}
+      />
+      <Button className="custom-button" submits="true">
+        Search
+      </Button>
       <Button className="custom-button">Reset</Button>
     </form>
   );
